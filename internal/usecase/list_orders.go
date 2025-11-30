4f8bec064bc78fa9eb2111559a60fc3e17c635f0
Marcos Garcia <vinicius-garcia13@hotmail.com>
2025-11-30 13:10:44 -0300

    initial commit

---
+++ internal/usecase/list_orders.go
@@ -0,0 +1,39 @@
+package usecase
+
+import "github.com/mvr-garcia/go-clean-arch/internal/entity"
+
+type ListOrdersOutputDTO struct {
+	Orders []OrderOutputDTO `json:"orders"`
+}
+
+type ListOrdersUseCase struct {
+	OrderRepository entity.OrderRepositoryInterface
+}
+
+func NewListOrdersUseCase(
+	OrderRepository entity.OrderRepositoryInterface,
+) *ListOrdersUseCase {
+	return &ListOrdersUseCase{
+		OrderRepository: OrderRepository,
+	}
+}
+
+func (l *ListOrdersUseCase) Execute() (ListOrdersOutputDTO, error) {
+	orders, err := l.OrderRepository.FindAll()
+	if err != nil {
+		return ListOrdersOutputDTO{}, err
+	}
+
+	var ordersDTO []OrderOutputDTO
+	for _, order := range orders {
+		orderDTO := OrderOutputDTO{
+			ID:         order.ID,
+			Price:      order.Price,
+			Tax:        order.Tax,
+			FinalPrice: order.Price + order.Tax,
+		}
+		ordersDTO = append(ordersDTO, orderDTO)
+	}
+
+	return ListOrdersOutputDTO{Orders: ordersDTO}, nil
+}
